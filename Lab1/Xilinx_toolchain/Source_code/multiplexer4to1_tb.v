bbffcc9ba0658f4578f23c0024836e37a272bb6c
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2012-04-21 04:35:04 +0800

    新增Lab1

---
+++ Lab1/Xilinx_toolchain/Source_code/multiplexer4to1_tb.v
@@ -0,0 +1,34 @@
+`timescale 1ns / 100ps
+`include "Source_code/multiplexer4to1/multiplexer4to1.v"
+module multiplexer4to1test;
+  reg [1:0] s;
+  reg [3:0] i;
+  wire out;
+  multiplexer4to1 
+    mul41(.out(out), 
+          .s(s), 
+          .i(i));
+  initial 
+  begin
+    //初始化
+    $dumpfile("Simulation/multiplexer4to1_tb.vcd");
+    $dumpvars;
+    $monitor($time, " in = %b%b%b%b, select = %b%b -> out = %b", i[3], i[2], i[1], i[0], s[1], s[0], out);
+		s = 2'b00;
+		i = 4'b0000;
+		
+		//模擬
+    #10 i = 4'b0001;
+    //output should now 1
+    
+    #10 s = 2'b01;
+    #10 i = 4'b0010;
+    
+    #10 s = 2'b10;
+    #10 i = 4'b0100;
+    
+    #10 s = 2'b11;
+    #10 i = 4'b1000;
+    #10 $finish;
+  end
+endmodule
