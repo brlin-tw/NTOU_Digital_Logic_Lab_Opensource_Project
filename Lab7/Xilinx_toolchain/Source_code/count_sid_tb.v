8f4e3c552637ada02b66e4aa8bd688e0d2cc355c
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2012-06-13 10:42:13 +0800

    [Lab7]完成Xilinx Toolchain

--- Lab7/Xilinx_toolchain/Source_code/count_sid_tb.v
+++ Lab7/Xilinx_toolchain/Source_code/count_sid_tb.v
@@ -1,31 +1,31 @@
 //testbench範本
 `timescale 1ns / 100ps
 
-`include "Source_code/dff_counter_sid_top.v"
+`include "Source_code/count_sid.v"
 
 //時脈頻率
 `define CLOCK_FREQ 10
 
 module dff_counter_sid_top_tb();
   reg Clk, rst_n;
-  wire [7:0]seg;
+  wire [3:0]sid;
 
 	//D.U.T. instantiation
-	dff_counter_sid_top dff_sid_top(seg, Clk, rst_n);
+	count_sid test(sid, Clk, rst_n);
 
 	//初始化
   initial
     begin
-      $dumpfile ("Simulation/dff_counter_sid_top_tb.vcd");
+      $dumpfile ("Simulation/count_sid_tb.vcd");
       $dumpvars;
-			$monitor($time, " out=%7b, <=== clk=%b, rst_n=%b", seg, Clk, rst_n);
+			$monitor($time, " out=%4b, <=== clk=%b, rst_n=%b", sid, Clk, rst_n);
 			Clk<=0;
 			rst_n<=0;
 
 			#15
 			rst_n = 1;
-
-			#160
+			
+			#105
 			$finish;
     end
 
